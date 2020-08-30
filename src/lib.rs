de504e1c15bc02c05fd29ee792db29a099dd8d54
Jonathan Zrake <jonathan.zrake@gmail.com>
2020-08-30 15:07:50 -0400

    Document methods and structs

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,10 @@ use std::fmt;
 
 
 
-// ============================================================================
+/**
+ * Enum (variant) whose kind is either bool, int, float, or string. These are
+ * the types of values allowed in a Form.
+ */
 #[derive(Clone)]
 pub enum Value {
     B(bool),
@@ -19,6 +22,10 @@ impl From<f64>  for Value { fn from(a: f64)  -> Self { Value::F(a) } }
 impl From<&str> for Value { fn from(a: &str) -> Self { Value::S(a.into()) } }
 
 impl Value {
+
+    /**
+     * Determine whether this value and another are of the same kind.
+     */
     pub fn same_kind_as(&self, other: &Value) -> bool {
        match (&self, &other) {
            (Value::B(_), Value::B(_)) => true,
@@ -49,16 +56,6 @@ impl fmt::Display for Value {
 
 
 // ============================================================================
-#[derive(Clone)]
-pub struct Parameter {
-    pub value: Value,
-    pub about: String,
-}
-
-
-
-
-// ============================================================================
 #[derive(Debug)]
 pub struct ConfigError
 {
@@ -83,7 +80,24 @@ impl std::error::Error for ConfigError {}
 
 
 
-// ============================================================================
+/**
+ * A value and an about string. This is the value type of the HashMap used in a
+ * Form.
+ */
+#[derive(Clone)]
+pub struct Parameter {
+    pub value: Value,
+    pub about: String,
+}
+
+
+
+
+/**
+ * A configuration data structure that is kind-checked at runtime. Items are
+ * declared using the `item` member function, after which their value can be
+ * updated but their kind (bool, int, float, string) cannot change.
+ */
 pub struct Form {
     parameter_map: HashMap::<String, Parameter>
 }
@@ -93,16 +107,39 @@ pub struct Form {
 
 // ============================================================================
 impl Form {
+
+    /**
+     * Create a blank form
+     */
     pub fn new() -> Form {
         Form{parameter_map: HashMap::new()}
     }
 
+    /**
+     * Declare a new config item. Any item already declared with that name is
+     * replaced.
+     *
+     * # Arguments
+     *
+     * * `key`     - The name of the config item
+     * * `default` - The default value
+     * * `about`   - A description of the item for use in user reporting
+     */
     pub fn item<T: Into<Value>>(&self, key: &str, default: T, about: &str) -> Self {
         let mut parameter_map = self.parameter_map.clone();
         parameter_map.insert(key.into(), Parameter{value: default.into(), about: about.into()});
         Form{parameter_map: parameter_map}
     }
 
+    /**
+     * Merge in the contents of a string-value map. The result is an error if
+     * any of the new keys have not already been declared in the form, or if
+     * they were declared as a different type.
+     *
+     * # Arguments
+     *
+     * * `items` - A map of values to update the map with
+     */
     pub fn merge_value_map(&self, items: &HashMap<String, Value>) -> Result<Self, ConfigError> {
         let mut parameter_map = self.parameter_map.clone();
         for (key, new_value) in items {
@@ -118,6 +155,15 @@ impl Form {
         Ok(Form{parameter_map: parameter_map})
     }
 
+    /**
+     * Merge in the contents of a string-string map. The result is an error if
+     * any of the new keys have not already been declared in the form, or if
+     * any of the value strings do not parse to the declared type.
+     *
+     * # Arguments
+     *
+     * * `dict` - A map of string to update the map with
+     */
     pub fn merge_string_map(&self, dict: HashMap<String, String>) -> Result<Self, ConfigError> {
         use Value::*;
         let mut parameter_map = self.parameter_map.clone();
@@ -134,6 +180,18 @@ impl Form {
         Ok(Form{parameter_map: parameter_map})
     }
 
+    /**
+     * Get an item from the form. Panics if the item was not declared.
+     * 
+     * # Arguments
+     * 
+     * * `key` - The key to get
+     * 
+     * # Example
+     * ```compile_fail
+     * let x = form.get("counter").to_int(); // fails unless counter is declared and is i64
+     * ```
+     */
     pub fn get(&self, key: &str) -> &Value {
         &self.parameter_map.get(key.into()).unwrap().value
     }
